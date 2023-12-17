8c8c5c79ffa97d518ae55feed8d69c57974a73a2
Slawomir Wegrzyn <slawek.info@gmail.com>
2023-12-17 11:22:30 -0600

    fx rates

--- bankier/src/main/scala/pl/invest/WebInteractions.scala
+++ bankier/src/main/scala/pl/invest/WebInteractions.scala
@@ -12,6 +12,7 @@ import java.sql.{Connection, DriverManager}
 import java.time.format.DateTimeFormatter
 import java.time.{Duration, LocalDate, LocalDateTime, Period}
 import java.util.function.Consumer
+import scala.collection.immutable.LazyList
 import scala.io.Source
 import scala.util.Using
 
@@ -45,13 +46,28 @@ object WebInteractions {
     initializeInactiveShares(args(1))
     initializeDbTrades(args(0))
     initializeDbPrices()
+    initializeFxRates("USD", "PLN")
   }
 
-  def getUrlContent(url: URL): String = {
-    val source = Source.fromURL(url)
-    val content = source.mkString
-    source.close()
-    content
+  private def initializeFxRates(from: String, to: String): Unit = {
+    getFTFxIdentifier(s"$from$to").foreach { symbol =>
+
+      val tickersAndDates: LazyList[(String, LocalDate, String, BigDecimal)] = tickerTradeDates
+      val startDate = tickersAndDates.head._2
+
+      inDb("Fx rates initialized")(connection => {
+        val statement = connection.createStatement()
+        statement.executeUpdate("CREATE TABLE fx_rates (fromCcy CHAR(3), toCcy CHAR(3), date DATE, rate DECIMAL(10,2))")
+        getFxRatesFTOf(symbol, startDate).map { content =>
+          println(s"currency pair: $from$to, date: ${content._1}, rate: ${content._2}")
+          val sqlInsert = s"INSERT INTO fx_rates VALUES (" +
+            s"'$from', s'$to', '${content._1.format(DateTimeFormatter.ISO_DATE)}', ${content._2})"
+
+          println(s"Inserting fx rate: $sqlInsert")
+          statement.executeUpdate(sqlInsert)
+        }
+      })
+    }
   }
 
   private def initializeInactiveShares(filePath: String): Unit = {
@@ -111,64 +127,70 @@ object WebInteractions {
 
   private def initializeDbPrices(): Unit = {
 
+    val tickersAndDates: LazyList[(String, LocalDate, String, BigDecimal)] = tickerTradeDates
+
+    val startDate = tickersAndDates.head._2
+    inDb("Store shares prices")(connection => {
+      val statement = connection.createStatement()
+      val prices = tickersAndDates.map {
+        case (ticker, _, market, startPrice) => (s"$ticker:${exchanges(market)(ticker)}", startPrice)
+      }.foldLeft(Map.empty[String, BigDecimal]) {
+        case (acc, (ticker, startPrice)) if !acc.contains(ticker) => acc.updated(ticker, startPrice)
+        case (acc, _) => acc
+      }.foldLeft(Map.empty[String, Seq[(LocalDate, BigDecimal)]]) {
+        case (prices, (ticker, _)) if !inactive(ticker) =>
+          getFTIdentifier(ticker) match {
+            case Some(ftIdentifier) => prices ++ getPricesFTOf(ftIdentifier, ticker, startDate)
+            case None => throw new RuntimeException(s"Ticker's $ticker FT id not found")
+          }
+        case (prices, (ticker, startPrice)) =>
+          prices.find(_._2.nonEmpty).map {
+            case (_, details) =>
+              prices ++ Map(ticker -> details.map(d => (d._1, startPrice)))
+          }.getOrElse(prices)
+      }
+
+      statement.executeUpdate("CREATE TABLE prices (ticker VARCHAR(255), currency CHAR(3), date DATE, close DECIMAL(10,2), volume INT)")
+
+      prices.foreachEntry {
+        case (ticker, details) =>
+          val equity = ticker.substring(0, ticker.indexOf(":"))
+          val currency = currencyExchangeMap(ticker.substring(ticker.indexOf(":") + 1))
+          details.foreach {
+            case (date, close) =>
+              val sqlInsert = s"INSERT INTO prices VALUES (" +
+                s"'$equity', '$currency', '${date.format(DateTimeFormatter.ISO_DATE)}', $close, -1)"
+
+              println(s"Inserting price: $sqlInsert")
+
+              statement.executeUpdate(sqlInsert)
+          }
+      }
+    })
+  }
+
+  private def tickerTradeDates: LazyList[(String, LocalDate, String, BigDecimal)] = {
+    var tickersAndTradeDates: LazyList[(String, LocalDate, String, BigDecimal)] = LazyList.empty
     inDb("Dates and shares read")(connection => {
       val statement = connection.createStatement()
-      val resultSet = statement.executeQuery("SELECT DISTINCT ticker, settledDate, chamber, price FROM trades ORDER BY settledDate")
-      val tickersAndDates = new Iterator[(String, LocalDate, String, BigDecimal)] {
+      val resultSet = statement.executeQuery("SELECT DISTINCT ticker, realizationTime, chamber, price FROM trades ORDER BY realizationTime ASC")
+      tickersAndTradeDates = new Iterator[(String, LocalDate, String, BigDecimal)] {
         def hasNext: Boolean = resultSet.next()
 
         def next(): (String, LocalDate, String, BigDecimal) = (
           resultSet.getString("ticker"),
-          resultSet.getDate("settledDate").toLocalDate,
+          resultSet.getTimestamp("realizationTime").toLocalDateTime.toLocalDate,
           resultSet.getString("chamber"),
           resultSet.getBigDecimal("price")
         )
       }.to(LazyList)
-
-      val startDate = tickersAndDates.head._2
-      inDb("Store shares prices")(connection => {
-        val statement = connection.createStatement()
-        val prices = tickersAndDates.map {
-          case (ticker, _, market, startPrice) => (s"$ticker:${exchanges(market)(ticker)}", startPrice)
-        }.foldLeft(Map.empty[String, BigDecimal]) {
-          case (acc, (ticker, startPrice)) if !acc.contains(ticker) => acc.updated(ticker, startPrice)
-          case (acc, _) => acc
-        }.foldLeft(Map.empty[String, Seq[(LocalDate, BigDecimal)]]) {
-          case (prices, (ticker, _)) if !inactive(ticker) =>
-            getFTIdentifier(ticker) match {
-              case Some(ftIdentifier) => prices ++ getPricesFTOf(ftIdentifier, ticker, startDate)
-              case None => throw new RuntimeException(s"Ticker's $ticker FT id not found")
-            }
-          case (prices, (ticker, startPrice)) =>
-            prices.find(_._2.nonEmpty).map {
-              case (_, details) =>
-                prices ++ Map(ticker -> details.map(d => (d._1, startPrice)))
-            }.getOrElse(prices)
-        }
-
-        statement.executeUpdate("CREATE TABLE prices (ticker VARCHAR(255), currency CHAR(3), date DATE, close DECIMAL(10,2), volume INT)")
-
-        prices.foreachEntry {
-          case (ticker, details) =>
-            val equity = ticker.substring(0, ticker.indexOf(":"))
-            val currency = currencyExchangeMap(ticker.substring(ticker.indexOf(":") + 1))
-            details.foreach {
-              case (date, close) =>
-                val sqlInsert = s"INSERT INTO prices VALUES (" +
-                  s"'$equity', '$currency', '${date.format(DateTimeFormatter.ISO_DATE)}', $close, -1)"
-
-                println(s"Inserting price: $sqlInsert")
-
-                statement.executeUpdate(sqlInsert)
-            }
-        }
-      })
     })
+    tickersAndTradeDates
   }
 
   private def inactive(ticker: String): Boolean = inactiveShares.contains(ticker)
 
-  def getFTIdentifier(ticker: String): Option[String] = {
+  private def getFTIdentifier(ticker: String): Option[String] = {
     print(s"Ticker: $ticker ...")
     val url = new URL(s"https://markets.ft.com/data/equities/tearsheet/summary?s=$ticker")
     val content = getUrlContent(url)
@@ -186,7 +208,25 @@ object WebInteractions {
         None
   }
 
-  def getPricesFTOf(symbol: String, ticker: String, startDate: LocalDate): Map[String, Seq[(LocalDate, BigDecimal)]] = {
+  private def getFTFxIdentifier(ticker: String): Option[String] = {
+    print(s"Fx pair: $ticker ...")
+    val url = new URL(s"https://markets.ft.com/data/currencies/tearsheet/summary?s=$ticker")
+    val content = getUrlContent(url)
+    val divIndex = content.indexOf("<div data-module-name=\"SymbolChartApp\"") + "data-module-name=\"SymbolChartApp\"".length
+    val section = content.substring(divIndex, content.indexOf(">", divIndex + 1)).replaceAll("&quot;", "'")
+
+    Option(section.substring(
+      section.indexOf("'issueId':") + "'issueId':".length,
+      section.indexOf(",", section.indexOf("'issueId':") + "'issueId':".length - 1)
+    )).filter(_.nonEmpty).map(_.replaceAll("'", "")).filter(v => StringUtils.isNumber(v)) match
+      case Some(ftIdentifier) => Some(ftIdentifier)
+      case None =>
+        println(s"FT Fx identifier not found for ticker $ticker: \n$content")
+        None
+
+  }
+
+  private def getPricesFTOf(symbol: String, ticker: String, startDate: LocalDate): Map[String, Seq[(LocalDate, BigDecimal)]] = {
     print(s" $symbol ...")
     val period = Period.between(startDate, LocalDate.now())
     val days = period.getDays + period.getMonths * 30 + period.getYears * 365
@@ -236,7 +276,58 @@ object WebInteractions {
     }
   }
 
-  protected def connectPostToHttpServerAndReadResponse(request: HttpRequest): String = {
+  private def getFxRatesFTOf(symbol: String, startDate: LocalDate): Seq[(LocalDate, BigDecimal)] = {
+    print(s" $symbol ...")
+    val period = Period.between(startDate, LocalDate.now())
+    val days = period.getDays + period.getMonths * 30 + period.getYears * 365
+
+    val request = HttpRequest.newBuilder
+      .uri(URI.create(s"https://markets.ft.com/data/chartapi/series"))
+      .header("Content-Type", "application/json")
+      .header("Charset", "UTF-8")
+      .POST(HttpRequest.BodyPublishers.ofString(
+        s"""{
+           |  "days": $days,
+           |  "dataNormalized": false,
+           |  "dataPeriod": "Day",
+           |  "dataInterval": 1,
+           |  "realtime": false,
+           |  "yFormat": "0.###",
+           |  "timeServiceFormat": "JSON",
+           |  "rulerIntradayStart": 26,
+           |  "rulerIntradayStop": 3,
+           |  "rulerInterdayStart": 10957,
+           |  "rulerInterdayStop": 365,
+           |  "returnDateType": "ISO8601",
+           |  "elements": [
+           |    {
+           |      "Label": "62d3d51b",
+           |      "Type": "price",
+           |      "Symbol": "$symbol",
+           |      "OverlayIndicators": [],
+           |      "Params": {}
+           |    }
+           |  ]
+           |}""".stripMargin))
+      .build
+
+    val response: String = connectPostToHttpServerAndReadResponse(request)
+
+    parse(response) match {
+      case Left(_) => Seq[(LocalDate, BigDecimal)]()
+      case Right(ftSharePriceJson) =>
+        val cursor: HCursor = ftSharePriceJson.hcursor
+        val dates = cursor.downField("Dates").as[Seq[String]].getOrElse(Seq.empty[String])
+        val prices = cursor.downField("Elements").downN(0).downField("ComponentSeries").downN(3).downField("Values").as[Seq[BigDecimal]].getOrElse(Seq.empty[BigDecimal])
+        val ccy = s"${cursor.downField("Elements").downN(0).downField("Currency")}"
+        println(s" $ccy: ${dates.length} dates, ${prices.length} prices")
+        dates.zip(prices).map {
+          case (date, price) => (LocalDate.parse(date.substring(0, 10), DateTimeFormatter.ISO_DATE), price)
+        }
+    }
+  }
+
+  private def connectPostToHttpServerAndReadResponse(request: HttpRequest): String = {
     val client = HttpClient.newBuilder
       .version(Version.HTTP_2)
       .followRedirects(Redirect.NORMAL)
@@ -253,5 +344,12 @@ object WebInteractions {
     }.toEither match
       case Left(exception) => println(exception); exception.printStackTrace()
       case Right(_) => println(s"$operationName finished")
+  }
+
+  private def getUrlContent(url: URL): String = {
+    val source = Source.fromURL(url)
+    val content = source.mkString
+    source.close()
+    content
   }
 }
